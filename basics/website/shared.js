cde382406ffb8b768455a8c335411fbf5c2600a7
Mohanavelu Subramanian <mhnvelu@gmail.com>
2020-05-19 00:05:00 +0100

    advanced : website v6 - Animation events in JS

--- basics/website/shared.js
+++ basics/website/shared.js
@@ -4,6 +4,7 @@ var modal = document.querySelector(".modal");
 var selectPlanButtons = document.querySelectorAll(".plan button");
 var toggleButton = document.querySelector(".toggle-button");
 var mobileNav = document.querySelector(".mobile-nav");
+var ctaButton = document.querySelector(".main-nav__item--cta");
 
 for (var i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener("click", () => {
@@ -40,3 +41,16 @@ function closeModal() {
   }
   backdrop.classList.remove("open");
 }
+
+// The below methods are executed when animation happens on cta button.
+ctaButton.addEventListener("animationstart", function (event) {
+  console.log("Animation started", event);
+});
+
+ctaButton.addEventListener("animationend", function (event) {
+  console.log("Animation ended", event);
+});
+
+ctaButton.addEventListener("animationiteration", function (event) {
+  console.log("Animation iteration", event);
+});
